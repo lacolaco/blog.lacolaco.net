a41a600d8b68730c8f69525e2033ed61717ee575
Suguru Inatomi <suguru.inatomi@gmail.com>
2021-03-21 18:43:30 +0900

    feat: switch highlight.js to prismjs (#13)

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-import './src/js/hljs';
+import './src/js/syntax-highlight';
 import './src/js/anchorjs';
 import './src/js/katex';
---
+++ src/js/syntax-highlight.js
@@ -0,0 +1,10 @@
+import 'prismjs';
+import 'prismjs/plugins/autoloader/prism-autoloader';
+import 'prismjs/plugins/toolbar/prism-toolbar';
+import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
+
+import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-markdown';
+import 'prismjs/components/prism-scss';
+import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-yaml';
