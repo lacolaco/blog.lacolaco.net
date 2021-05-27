826b14ee198ad1fbfac8fc1075590d5de53e3075
Suguru Inatomi <suguru.inatomi@gmail.com>
2021-05-27 23:23:09 +0900

    fix: remove elements project

--- elements/apps/elements/src/app/shared/icons/favorite-border/favorite-border.component.ts
+++
@@ -1,8 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'elements-favorite-border-icon',
-  templateUrl: './favorite-border.component.svg',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class FavoriteBorderIconComponent {}
--- elements/apps/elements/src/app/shared/icons/favorite/favorite.component.ts
+++
@@ -1,8 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'elements-favorite-icon',
-  templateUrl: './favorite.component.svg',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class FavoriteIconComponent {}
