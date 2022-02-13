830cf0f6697fbeeedfe8d62319afda6eb65a6afd
Suguru Inatomi <suguru.inatomi@gmail.com>
2022-02-14 01:02:02 +0900

    feat: support bookmark and callout blocks (#124)

--- tools/lib/posts/markdown.ts
+++ tools/lib/posts/markdown.ts
@@ -14,6 +14,8 @@ type RenderableBlockObjectType = Extract<
   | 'numbered_list_item'
   | 'quote'
   | 'divider'
+  | 'bookmark'
+  | 'callout'
 >;
 type BlockObjectRenderer<T extends BlockObjectType> = (
   block: MatchType<BlockObject, { type: T }>,
@@ -29,23 +31,16 @@ export async function renderContentMarkdown(
   externalImageResolver: ExternalImageResolver,
 ): Promise<string> {
   const renderer: BlockObjectRendererMap = {
-    heading_1: (block) => `# ${block.heading_1.text[0].plain_text}\n\n`,
-    heading_2: (block) => `## ${block.heading_2.text[0].plain_text}\n\n`,
-    heading_3: (block) => `### ${block.heading_3.text[0].plain_text}\n\n`,
+    heading_1: (block) => `# ${renderRichTextArray(block.heading_1.text)}\n\n`,
+    heading_2: (block) => `## ${renderRichTextArray(block.heading_2.text)}\n\n`,
+    heading_3: (block) => `### ${renderRichTextArray(block.heading_3.text)}\n\n`,
     paragraph: (block) => {
       return `${block.paragraph.text
         .map((node) => {
           if (node.type !== 'text') {
             return '';
           }
-          if (node.plain_text === '\n') {
-            return '  \n';
-          }
-          const text = renderTextWithAnnotation(node.plain_text, node.annotations);
-          if (node.href) {
-            return `[${text}](${node.href})`;
-          }
-          return text;
+          return renderRichText(node);
         })
         .join('')}\n\n`;
     },
@@ -55,7 +50,6 @@ export async function renderContentMarkdown(
           return `![${block.image.caption}](${block.image.external.url})\n\n`;
         case 'file':
           const imagePath = await externalImageResolver(block.image.file.url);
-          // TODO: download file and replace with relative path
           return `![${block.image.caption}](/img/${imagePath})\n\n`;
       }
     },
@@ -66,25 +60,29 @@ export async function renderContentMarkdown(
     },
     divider: () => '---\n\n',
     quote: (block) => {
-      return `> ${block.quote.text
-        .map((node) => renderTextWithAnnotation(node.plain_text, node.annotations))
-        .join('')}\n\n`;
+      return `> ${renderRichTextArray(block.quote.text)}\n\n`;
     },
     bulleted_list_item: (block) => {
       const indent = '\t'.repeat(block.depth);
-      const text = renderTextWithAnnotation(
-        block.bulleted_list_item.text[0].plain_text,
-        block.bulleted_list_item.text[0].annotations,
-      );
+      const text = renderRichTextArray(block.bulleted_list_item.text);
       return `${indent}- ${text}\n`;
     },
     numbered_list_item: (block) => {
       const indent = '\t'.repeat(block.depth);
-      const text = renderTextWithAnnotation(
-        block.numbered_list_item.text[0].plain_text,
-        block.numbered_list_item.text[0].annotations,
-      );
+      const text = renderRichTextArray(block.numbered_list_item.text);
       return `${indent}1. ${text}\n`;
+    },
+    bookmark: (block) => {
+      return `{{< embed "${block.bookmark.url}" >}}\n\n`;
+    },
+    callout: (block) => {
+      const emojiIcon = block.callout.icon?.type === 'emoji' ? block.callout.icon.emoji : null;
+      const text = renderRichTextArray(block.callout.text);
+      if (emojiIcon) {
+        return `{{< callout "${emojiIcon}">}}\n${text}\n{{< /callout >}}\n\n`;
+      } else {
+        return `{{< callout >}}\n${text}\n{{< /callout >}}\n\n`;
+      }
     },
   } as const;
 
@@ -105,33 +103,63 @@ export async function renderContentMarkdown(
   return (await Promise.all(content.map(walk))).join('');
 }
 
-function renderTextWithAnnotation(
-  text: string,
+type RichTextObject = {
+  plain_text: string;
+  href: string | null;
   annotations: {
     bold: boolean;
     italic: boolean;
     strikethrough: boolean;
     underline: boolean;
     code: boolean;
-  },
-): string {
+  };
+};
+
+function renderRichTextArray(array: RichTextObject[]): string {
+  return array.map(renderRichText).join('');
+}
+
+function renderRichText(richText: RichTextObject): string {
+  const { plain_text, href, annotations } = richText;
   if (annotations.code) {
-    return `\`${text}\``;
+    return `\`${plain_text}\``;
   }
   if (annotations.bold) {
-    return renderTextWithAnnotation(`**${text}**`, { ...annotations, bold: false });
+    return renderRichText({
+      ...richText,
+      plain_text: `**${plain_text}**`,
+      annotations: { ...annotations, bold: false },
+    });
   }
   if (annotations.italic) {
-    const mark = text.startsWith('*') ? '_' : '*';
-    return renderTextWithAnnotation(`${mark}${text}${mark}`, { ...annotations, italic: false });
+    const mark = plain_text.startsWith('*') ? '_' : '*';
+    return renderRichText({
+      ...richText,
+      plain_text: `${mark}${plain_text}${mark}`,
+      annotations: { ...annotations, italic: false },
+    });
   }
   if (annotations.strikethrough) {
-    return renderTextWithAnnotation(`~~${text}~~`, { ...annotations, strikethrough: false });
+    return renderRichText({
+      ...richText,
+      plain_text: `~~${plain_text}~~`,
+      annotations: { ...annotations, strikethrough: false },
+    });
   }
   if (annotations.underline) {
-    return renderTextWithAnnotation(`__${text}__`, { ...annotations, underline: false });
+    return renderRichText({
+      ...richText,
+      plain_text: `__${plain_text}__`,
+      annotations: { ...annotations, underline: false },
+    });
   }
-  return text;
+  if (href) {
+    return renderRichText({ ...richText, plain_text: `[${plain_text}](${href})`, href: null });
+  }
+  if (plain_text.includes('\n')) {
+    return plain_text.replace(/\n/g, '  \n');
+  }
+  return plain_text;
 }
 
 export function renderFrontmatter(params: Record<string, unknown>): string {
