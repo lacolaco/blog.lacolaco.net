2ae151d92a29670abc248de8927678a6fc308a41
Suguru Inatomi <suguru.inatomi@gmail.com>
2023-06-23 15:42:03 +0900

    refactor: remove primer css dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,24 +3,11 @@ import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import rehypeSlug from 'rehype-slug';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.lacolaco.net',
   integrations: [mdx(), sitemap(), tailwind(), react()],
-  markdown: {
-    rehypePlugins: [
-      rehypeSlug,
-      [
-        rehypeAutolinkHeadings,
-        {
-          behavior: 'prepend',
-        },
-      ],
-    ],
-  },
   vite: {
     optimizeDeps: { exclude: ['@resvg/resvg-js'] },
   },
