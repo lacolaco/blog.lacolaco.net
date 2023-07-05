fd86a06897ce1d237c44daf5fa47a489499b27da
Suguru Inatomi <suguru.inatomi@gmail.com>
2023-07-05 17:26:38 +0900

    chore: remove mdx package (#426)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
@@ -7,7 +6,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.lacolaco.net',
-  integrations: [mdx(), sitemap(), tailwind(), react()],
+  integrations: [sitemap(), tailwind(), react()],
   vite: {
     optimizeDeps: { exclude: ['@resvg/resvg-js'] },
   },
