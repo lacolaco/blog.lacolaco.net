ce74f99c9859c3c84d581e2e0841979ba8da9e6b
Suguru Inatomi <suguru.inatomi@gmail.com>
2023-07-26 09:41:36 +0900

    fix: change blog title

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = '遠近法';
-export const SITE_DESCRIPTION = 'lacolaco のブログ';
+export const SITE_TITLE = 'Marginalia';
+export const SITE_DESCRIPTION = 'lacolaco のブログ。';
 export const SITE_COPYRIGHT = '© 2023 lacolaco.net';
