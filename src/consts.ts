6186cfa6ea6698957f172754e6b07874f87d913e
Suguru Inatomi <suguru.inatomi@gmail.com>
2023-08-04 08:37:07 +0900

    fix: display site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Marginalia';
-export const SITE_DESCRIPTION = 'lacolaco のブログ。';
+export const SITE_DESCRIPTION = 'lacolacoの余白は無限である';
 export const SITE_COPYRIGHT = '© 2023 lacolaco.net';
