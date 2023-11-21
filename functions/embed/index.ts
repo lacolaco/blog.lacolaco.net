c9b307f8d129ac2d5a86aa133094b1df57268c3b
Suguru Inatomi <suguru.inatomi@gmail.com>
2023-11-22 08:54:14 +0900

    chore: fetch page title

--- functions/embed/index.ts
+++ functions/embed/index.ts
@@ -5,5 +5,28 @@ export const onRequest: PagesFunction<Env> = async (context) => {
   if (!url) {
     return new Response('Missing url parameter', { status: 400 });
   }
-  return new Response(`url is ${url}!`);
+  const title = await getPageTitle(url);
+  return new Response(`url is ${url}, title is ${title}`);
 };
+
+class DocumentTitleHandler {
+  title: string;
+  element(element: HTMLTitleElement) {
+    this.title ??= element.text;
+  }
+}
+
+class OgTitleHandler {
+  title: string;
+  element(element: HTMLMetaElement) {
+    this.title ??= element.getAttribute('content');
+  }
+}
+
+async function getPageTitle(url: string) {
+  const response = await fetch(url);
+  const docTitle = new DocumentTitleHandler();
+  const ogTitle = new OgTitleHandler();
+  new HTMLRewriter().on('title', docTitle).on('meta[property="og:title"]', ogTitle).transform(response);
+  return ogTitle.title ?? docTitle.title ?? 'Untitled';
+}
