71b4a1d4de44b89912c73dc8db8e8a025960b94d
Suguru Inatomi <suguru.inatomi@gmail.com>
2023-11-22 08:27:38 +0900

    chore: receive url from query

--- functions/embed/index.ts
+++ functions/embed/index.ts
@@ -1,5 +1,9 @@
 interface Env {}
 
 export const onRequest: PagesFunction<Env> = async (context) => {
-  return new Response('Hello, world!');
+  const url = new URL(context.request.url).searchParams.get('url');
+  if (!url) {
+    return new Response('Missing url parameter', { status: 400 });
+  }
+  return new Response(`url is ${url}!`);
 };
