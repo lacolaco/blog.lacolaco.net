5576c1bdfd19f74c1dc5d5284a1a585c9fe2dbe9
Suguru Inatomi <suguru.inatomi@gmail.com>
2024-01-21 17:20:57 +0900

    fix: use notion-db package (#671)
    * fix: use notion-db package
    
    * chore: use auth token
    
    * chore: set token
    
    * chore: set registry settings
    
    * chore: use cache

--- src/libs/notion/utils.ts
+++ src/libs/notion/utils.ts
@@ -1,6 +1,6 @@
 import type { Client } from '@notionhq/client';
 import { createHash } from 'node:crypto';
-import type { BlockObject, DatabasePropertyConfigs, PageObject, PageProperty, QueryFilterObject } from './types';
+import type { BlogPostLocale, DatabasePropertyConfigs, PageObject, PageProperty } from './types';
 
 /**
  * If the page has slug property, return it.
@@ -15,62 +15,12 @@ export function getSlug(page: PageObject): string {
   return createHash('sha1').update(page.id).digest('hex').slice(0, 12);
 }
 
-export function getLocale(page: PageObject): string | undefined {
+export function getLocale(page: PageObject): BlogPostLocale | undefined {
   const { locale } = page.properties as { locale?: PageProperty<'select'> };
   if (locale && locale.select) {
-    return locale.select.name;
+    return locale.select.name as BlogPostLocale;
   }
   return undefined;
-}
-
-export async function queryAllPages(
-  client: Client,
-  databaseId: string,
-  filter: QueryFilterObject,
-): Promise<PageObject[]> {
-  const pages: PageObject[] = [];
-  let cursor = null;
-  do {
-    const { results, next_cursor, has_more } = await client.databases.query({
-      database_id: databaseId,
-      archived: false,
-      filter,
-      page_size: 100,
-      start_cursor: cursor ?? undefined,
-      sorts: [{ timestamp: 'created_time', direction: 'descending' }],
-    });
-    for (const page of results) {
-      if ('properties' in page) {
-        pages.push(page as PageObject);
-      }
-    }
-    cursor = has_more ? next_cursor : null;
-  } while (cursor !== null);
-  return pages;
-}
-
-export async function fetchChildBlocks(client: Client, parentId: string): Promise<BlockObject[]> {
-  const blocks: BlockObject[] = [];
-  let cursor = null;
-  do {
-    const { results, next_cursor, has_more } = await client.blocks.children.list({
-      block_id: parentId,
-      page_size: 100,
-      start_cursor: cursor ?? undefined,
-    });
-    for (const block of results) {
-      if ('type' in block) {
-        if (block.has_children) {
-          const children = await fetchChildBlocks(client, block.id);
-          blocks.push({ ...block, children });
-        } else {
-          blocks.push({ ...block });
-        }
-      }
-    }
-    cursor = has_more ? next_cursor : null;
-  } while (cursor !== null);
-  return blocks;
 }
 
 export async function fetchDatabaseProperties(client: Client, databaseId: string): Promise<DatabasePropertyConfigs> {
