2534e83d7469b762b0cdc092690bbdb000b1213d
Suguru Inatomi <suguru.inatomi@gmail.com>
2025-04-26 14:38:34 +0900

    refactor: upgrade tailwindcss to v4

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'lacolaco\'s marginalia';
+export const SITE_TITLE = "lacolaco's marginalia";
 export const SITE_DESCRIPTION = 'Reading and Developing';
 export const SITE_COPYRIGHT = '© 2023 lacolaco.net';
